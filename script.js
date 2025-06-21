7402cb1c762504b7e8570e2b44693ad75988a233
telole <asololearya@gmail.com>
2025-06-21 20:06:54 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const images = document.querySelectorAll('.image-gallery img');
+let currentIndex = 0;
+
+function showNextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add('active');
+}
+
+setInterval(showNextImage, 4000);
